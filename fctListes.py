50a6f1ba2700b1c05686e605cd47ba811b515665
1848857Cegepoutaouais <1848857@cegepoutaouais.qc.ca>
2025-10-02 16:46:20 -0400

    Changes by Jonathan 16:46

--- fctListes.py
+++ fctListes.py
@@ -12,8 +12,38 @@ qui retourne les points de vies restants après l'attaque.
 2. Fonction qui reçoit une liste de legos et une couleur et qui le pourcentage de blocs de cette couleur
 """
 
+#fonction pour le pourcentage
+    #boucle de 7 fois
+        #input de la couleur et ajoute à la liste
+    #demande la couleur
+    #nombre de lego avec la couleur dans la liste
+    #nombre total de lego
+    #return moyenne
+
+liste_pourcentage = [""]
+def pourcentage():
+    for i in range(8):
+        liste_pourcentage.append(str(input("ajouter un lego avec une couleur")))
+    choix_couleur_1 = str(input("quelle couleur est que vous voulez trouvez son pourcentage avec?"))
+    montant_lego_choisi = len(choix_couleur_1)
+    lego_total = len(liste_pourcentage)
+    pourcentage_couleur = 100*montant_lego_choisi/lego_total
+    return f"{pourcentage_couleur}%"
+
+
+
 """
 3. Fonction qui reçoit une liste de legos et qui il y a combien de blocs de chaque couleur en moyenne
+"""
+# Pseudo
+
+def moyenne():
+
+
+    return
+ls_lego = ["bleu", "jaune", "rouge", "vert", "bleu", "jaune"]
+len(ls_lego)
+
 """
 
 # Répartition des tâches :
